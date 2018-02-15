fa9b3c4f6109beb200abe7784cec4f82c7fcf17c
kshimabu <kshimabu@yahoo-corp.jp>
2018-02-15 18:15:29 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,9 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        lightsound
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <p class="subtitle">
+        島袋 光音
+      </p>
     </div>
   </section>
 </template>
